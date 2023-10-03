447ddf8746dcc201f284706d20cdb042941551c8
KonradMaslowiec <konrad.maslowiec@intive.com>
2023-10-03 16:17:46 +0200

    Simple Api Call

--- app/src/main/java/com/example/triviaapp/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/triviaapp/repository/QuestionRepository.kt
@@ -1,25 +1,21 @@
 package com.example.triviaapp.repository
 
-import android.util.Log
 import com.example.triviaapp.data.DataOrException
-import com.example.triviaapp.model.QuestionsItem
+import com.example.triviaapp.model.Questions
 import com.example.triviaapp.service.QuestionService
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val service: QuestionService) {
 
-    private val dataOrException = DataOrException<ArrayList<QuestionsItem>, Boolean, Exception>()
+    private val dataOrException = DataOrException<Questions, Boolean, Exception>()
 
-    suspend fun getQuestions(): DataOrException<ArrayList<QuestionsItem>, Boolean, Exception> {
+    suspend fun getQuestions(): DataOrException<Questions, Boolean, Exception> {
         try {
             dataOrException.loading = true
             dataOrException.data = service.getAllQuestions()
-            Log.d("Bughunt", "SUCCESS: $dataOrException.data")
             if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
         } catch (exception: Exception) {
             dataOrException.exception = exception
-            Log.d("Bughunt", "Failure: $dataOrException.data")
-
         }
         return dataOrException
     }
--- app/src/main/java/com/example/triviaapp/screens/viewmodel/QuestionViewModel.kt
+++ app/src/main/java/com/example/triviaapp/screens/viewmodel/QuestionViewModel.kt
@@ -3,7 +3,7 @@ package com.example.triviaapp.screens.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.triviaapp.model.QuestionsItem
+import com.example.triviaapp.model.Questions
 import com.example.triviaapp.repository.QuestionRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,7 +12,7 @@ import javax.inject.Inject
 @HiltViewModel
 class QuestionViewModel @Inject constructor(private val repository: QuestionRepository) : ViewModel() {
 
-    private val _questions = MutableLiveData<ArrayList<QuestionsItem>>()
+    private val _questions = MutableLiveData<Questions>()
     val question = _questions
 
     fun fetchQuestions() {
